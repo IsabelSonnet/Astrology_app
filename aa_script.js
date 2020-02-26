7ab1923495a9b10b721464ca6e8da6769043776c
Isabel  Sonnet <is73307@srcs.loc>
2020-02-26 13:52:36 -0800

    prob solving

--- aa_script.js
+++ aa_script.js
@@ -2,18 +2,17 @@ var button = document.getElementById("button");
 var result = document.getElementById("result");
 
 button.addEventListener("click", () => {
-    var bday = document.getElementById("bday").value;
-    var answer = findStarSign(bday);
+    var input = document.getElementById("dateInput").value;
+    var birthdate = new Date(input);
+    var answer = findStarSign(input);
 });
 
-function findStarSign(bday) {
-    var birthdate = new Date(bday);
-    var month = getMonth(birthdate);
-    var dayOfMonth = getDate(birthdate);
+function findStarSign() {
+    var month = birthdate.getMonth();
+    var dayOfMonth = birthdate.getDate();
     if (month == 1) {
         if (dayOfMonth <= 20) {
-            var capricorn = document.createElement("p");
-            result.appendChild(capricorn);
+            result.innerHTML = "<p>Capricorn</p>";
         } else {
             aquarius
         }
@@ -25,74 +24,75 @@ function findStarSign(bday) {
             pisces
         }
     }
-  if (month == 3) {
-      if (dayOfMonth <= 20){
-          pisces
-      } else {
-          aries
-      }
-  }
-  if (month == 4) {
-      if (dayOfMonth <= 19){
-          aries
-      } else {
-          taurus
-      }
-  }
-  if (month == 5) {
-      if (dayOfMonth <= 20){
-          taurus
-      } else {
-          gemini
-      }
-  }
-  if (month == 6) {
-      if (dayOfMonth <= 21){
-          gemini
-      } else {
-          cancer
-      }
-  }
-  if (month == 7) {
-      if (dayOfMonth <= ){
-          capricon
-      } else {
-          aquarius
-      }
-  }
-  if (month == 8) {
-      if (dayOfMonth <= 22){
-          return "leo";
-      } else {
-          return "virgo";
-      }
-  }
-   if (month == 9) {
-      if (dayOfMonth <= 22){
-          return "virgo";
-      } else {
-          return "libra";
-      }
-  }
-   if (month == 10) {
-      if (dayOfMonth <= 22){
-          return "libra";
-      } else {
-          return "scorpio"
-      }
-  }
-   if (month == 11) {
-      if (dayOfMonth <= 21){
-         return "scorpio";
-      } else {
-          return "sagittarius";
-      }
-  }
-   if (month == 12) {
-      if (dayOfMonth <= 21){
-          return "sagittarius";
-      } else {
-          return "capricorn";
-      }
-  }
+    if (month == 3) {
+        if (dayOfMonth <= 20) {
+            pisces
+        } else {
+            aries
+        }
+    }
+    if (month == 4) {
+        if (dayOfMonth <= 19) {
+            aries
+        } else {
+            taurus
+        }
+    }
+    if (month == 5) {
+        if (dayOfMonth <= 20) {
+            taurus
+        } else {
+            gemini
+        }
+    }
+    if (month == 6) {
+        if (dayOfMonth <= 21) {
+            gemini
+        } else {
+            cancer
+        }
+    }
+    if (month == 7) {
+        if (dayOfMonth <= ) {
+            capricon
+        } else {
+            aquarius
+        }
+    }
+    if (month == 8) {
+        if (dayOfMonth <= 22) {
+            return "leo";
+        } else {
+            return "virgo";
+        }
+    }
+    if (month == 9) {
+        if (dayOfMonth <= 22) {
+            return "virgo";
+        } else {
+            return "libra";
+        }
+    }
+    if (month == 10) {
+        if (dayOfMonth <= 22) {
+            return "libra";
+        } else {
+            return "scorpio"
+        }
+    }
+    if (month == 11) {
+        if (dayOfMonth <= 21) {
+            return "scorpio";
+        } else {
+            result.innerHTML = "<p>Capricorn</p>";
+        }
+    }
+    if (month == 12) {
+        if (dayOfMonth <= 21) {
+            result.innerHTML = "<p>Capricorn</p>";
+        } else {
+            return "capricorn";
+        }
+    }
 }
+
